25f0dd456d9d6598c38c62a20631fb8c6afec2ee
imthaghost <imthaghost@protonmail.com>
2019-10-30 11:04:31 -0700

    [update] Implementation of Vose Alias Method

--- src/dictogram.py
+++ src/dictogram.py
@@ -1,63 +1,212 @@
 #!python
+import os
+import re  # regular expression library
+from decimal import *
+from random import random
+from dictionary_words import time_it
 
-from __future__ import division, print_function  # Python 2 and 3 compatibility
+
+# from __future__ import division, print_function  # Python 2 and 3 compatibility
 
 
 class Dictogram(dict):
     """Dictogram is a histogram implemented as a subclass of the dict type."""
 
-    def __init__(self, word_list=None):
-        """Initialize this histogram as a new dict and count given words."""
+    def __init__(self, path=None):
+        """ Initialize this histogram as a new dict and count given words.
+            returns a histogram data structure that stores each unique word along with
+            the number of times that the word appears in the source text
+
+        Parameters
+        ----------
+        path : str,
+            path to file that contains text you want to create a histogram for
+
+        Returns
+        ----------
+        Dictogram: dict,
+            key value pair - (the unique words and amount of times the word appears)
+
+        Raises
+        ------
+        TypeError
+            If ....
+        """
         super(Dictogram, self).__init__()  # Initialize this as a new dict
-        # Add properties to track useful word counts for this histogram
         self.types = 0  # Count of distinct word types in this histogram
         self.tokens = 0  # Total count of all word tokens in this histogram
-        self.word_count = 0
-        # Count words in given list, if any
-        if word_list is not None:
-            for word in word_list:
-                self.add_count(word)
+        self.word_count = 0  # word count
+        self.alias_initialisation()
+        self.table_prob_list = list(self.table_prob)
+        if path is not None:
+            some_words = []
+            # open the text file
+            with open(path, 'r') as f:
+                # convert all words to lowecase to catch exceptions and edge cases
+                words = f.read().lower()
+                # create a words list
+                words_list = re.sub(r'[^a-zA-Z\s]', '', words)
+                # reassign the list
+                some_words = words_list.split()
+                self.word_count = len(some_words)
+            # find all words
+            for word in some_words:
+                if word:
+                    self[word] = self.get(word, 0) + 1
+        # after creating key-value pairs create instance variable that contains the sum of all values
+        self.sum = sum([self.get(key, 0) for key in self])  # sum of weights
+        # self.weights = [self.get(key, 0)/self.sum for key in self]
 
-    def add_count(self, word, count=1):
-        """Increase frequency count of given word by given count amount."""
-        # TODO: Increase word frequency by count
-        pass
+    def alias_initialisation(self):
+        """ Construct probability and alias tables for the distribution. """
+        # Initialise variables
+        n = len(self)
+        self.table_prob = {}   # probability table
+        self.table_alias = {}  # alias table
+        scaled_prob = {}       # scaled probabilities
+        small = []             # stack for probabilities smaller that 1
+        large = []             # stack for probabilities greater than or equal to 1
 
+        # Construct and sort the scaled probabilities into their appropriate stacks
+        for o, p in self.items():
+            scaled_prob[o] = Decimal(p) * n
+
+            if scaled_prob[o] < 1:
+                small.append(o)
+            else:
+                large.append(o)
+
+        # Construct the probability and alias tables
+        while small and large:
+            s = small.pop()
+            l = large.pop()
+
+            self.table_prob[s] = scaled_prob[s]
+            self.table_alias[s] = l
+
+            scaled_prob[l] = (scaled_prob[l] + scaled_prob[s]) - Decimal(1)
+
+            if scaled_prob[l] < 1:
+                small.append(l)
+            else:
+                large.append(l)
+
+        # The remaining outcomes (of one stack) must have probability 1
+        while large:
+            self.table_prob[large.pop()] = Decimal(1)
+
+        while small:
+            self.table_prob[small.pop()] = Decimal(1)
+
+        def alias_generation(self):
+            """ Return a random outcome from the distribution. """
+            # Determine which column of table_prob to inspect
+            col = random.choice(self.table_prob_list)
+
+            # Determine which outcome to pick in that column
+            if self.table_prob[col] >= random.uniform(0, 1):
+                return col
+            else:
+                return self.table_alias[col]
+
+    def sample_n(self, size):
+        """ Return a sample of size n from the distribution."""
+        # Ensure a non-negative integer as been specified
+        n = int(size)
+        if n <= 0:
+            raise ValueError(
+                "Please enter a non-negative integer for the number of samples desired: %d" % n)
+
+        return [self.alias_generation() for i in range(n)]
+
+    # HELPER FUNCTIONS
+
+    def get_words(self, file):
+        """ (str) -> list
+        Return a list of words from a given corpus. """
+
+        # Ensure the file is not empty
+        if os.stat(file).st_size == 0:
+            raise IOError(
+                "Please provide a file containing a corpus (not an empty file).")
+
+        # Ensure the file is text based (not binary). This is based on the implementation
+        #  of the Linux file command
+        textchars = bytearray([7, 8, 9, 10, 12, 13, 27]) + \
+            bytearray(range(0x20, 0x100))
+        with open(file, "rb") as bin_file:
+            if bool(bin_file.read(2048).translate(None, textchars)):
+                raise IOError(
+                    "Please provide a file containing text-based data.")
+
+        with open(file, "r") as corpus:
+            words = corpus.read().split()
+        return words
+
+    def sample2dist(self, sample):
+        """ (list) -> dict (i.e {outcome:proportion})
+        Construct a distribution based on an observed sample (e.g. rolls of a bias die) """
+        increment = Decimal(1)/len(sample)
+
+        dist = {}
+        get = dist.get
+        for o in sample:  # o for outcome
+            dist[o] = get(o, 0) + increment
+        return dist
+
+    @time_it
     def frequency(self, word):
-        """Return frequency count of given word, or 0 if word is not found."""
-        # TODO: Retrieve word frequency count'
-        pass
+        """returns the frequency in which a word is seen
+
+            Parameters
+            ----------
+            path : word,
+
+            Returns
+            ----------
+            historgram: dict,
+                key value pair - (the unique words and amount of times the word appears)
+        """
+        word = word.lower()
+        return self.get(word, 0)
+
+    def get_val(self, key):
+        return self[key]
+
+    @time_it
+    def prob(self, key):
+        x = self.get_val(key) / self.sum
+        return x
+
+    @time_it
+    def sample(self):
+        index = self.sum * random()
+        # for i in range(len(self.dictogram)):
+        #     if self.dictogram.keys
+
+        #     if compare < self.dictogram.items()[i]
+
+        return index
 
 
-def print_histogram(word_list):
-    print('word list: {}'.format(word_list))
-    # Create a dictogram and display its contents
-    histogram = Dictogram(word_list)
-    print('dictogram: {}'.format(histogram))
-    print('{} tokens, {} types'.format(histogram.tokens, histogram.types))
-    for word in word_list[-2:]:
-        freq = histogram.frequency(word)
-        print('{!r} occurs {} times'.format(word, freq))
-    print()
+# def print_histogram(word_list):
+#     print('word list: {}'.format(word_list))
+#     # Create a dictogram and display its contents
+#     histogram = Dictogram(word_list)
+#     print('dictogram: {}'.format(histogram))
+#     print('{} tokens, {} types'.format(histogram.tokens, histogram.types))
+#     for word in word_list[-2:]:
+#         freq = histogram.frequency(word)
+#         print('{!r} occurs {} times'.format(word, freq))
+#     print()
 
 
 def main():
-    import sys
-    arguments = sys.argv[1:]  # Exclude script name in first argument
-    if len(arguments) >= 1:
-        # Test histogram on given arguments
-        print_histogram(arguments)
-    else:
-        # Test histogram on letters in a word
-        word = 'abracadabra'
-        print_histogram(list(word))
-        # Test histogram on words in a classic book title
-        fish_text = 'one fish two fish red fish blue fish'
-        print_histogram(fish_text.split())
-        # Test histogram on words in a long repetitive sentence
-        woodchuck_text = ('how much wood would a wood chuck chuck'
-                          ' if a wood chuck could chuck wood')
-        print_histogram(woodchuck_text.split())
+    histogram = Dictogram('/usr/share/dict/words')
+    # print(histogram.frequency('boy'))
+    print(histogram.sum)
+    # print(histogram.sample())
+    print(histogram.prob('boy'))
 
 
 if __name__ == '__main__':
---
+++ src/vose.py
@@ -0,0 +1,163 @@
+import os
+import random
+import re
+import sys
+from decimal import *
+from optparse import OptionParser
+
+
+class VoseAlias(object):
+    """ A probability distribution for discrete weighted random variables and its probability/alias
+    tables for efficient sampling via Vose's Alias Method (a good explanation of which can be found at
+    http://www.keithschwarz.com/darts-dice-coins/).
+    """
+
+    def __init__(self, dist):
+        """ (VoseAlias, dict) -> NoneType """
+        self.dist = dist
+        self.alias_initialisation()
+        self.table_prob_list = list(self.table_prob)
+
+    def alias_initialisation(self):
+        """ Construct probability and alias tables for the distribution. """
+        # Initialise variables
+        n = len(self.dist)
+        self.table_prob = {}   # probability table
+        self.table_alias = {}  # alias table
+        scaled_prob = {}       # scaled probabilities
+        small = []             # stack for probabilities smaller that 1
+        large = []             # stack for probabilities greater than or equal to 1
+
+        # Construct and sort the scaled probabilities into their appropriate stacks
+        for o, p in self.dist.items():
+            scaled_prob[o] = Decimal(p) * n
+
+            if scaled_prob[o] < 1:
+                small.append(o)
+            else:
+                large.append(o)
+
+        # Construct the probability and alias tables
+        while small and large:
+            s = small.pop()
+            l = large.pop()
+
+            self.table_prob[s] = scaled_prob[s]
+            self.table_alias[s] = l
+
+            scaled_prob[l] = (scaled_prob[l] + scaled_prob[s]) - Decimal(1)
+
+            if scaled_prob[l] < 1:
+                small.append(l)
+            else:
+                large.append(l)
+
+        # The remaining outcomes (of one stack) must have probability 1
+        while large:
+            self.table_prob[large.pop()] = Decimal(1)
+
+        while small:
+            self.table_prob[small.pop()] = Decimal(1)
+
+    def alias_generation(self):
+        """ Return a random outcome from the distribution. """
+        # Determine which column of table_prob to inspect
+        col = random.choice(self.table_prob_list)
+
+        # Determine which outcome to pick in that column
+        if self.table_prob[col] >= random.uniform(0, 1):
+            return col
+        else:
+            return self.table_alias[col]
+
+    def sample_n(self, size):
+        """ Return a sample of size n from the distribution."""
+        # Ensure a non-negative integer as been specified
+        n = int(size)
+        if n <= 0:
+            raise ValueError(
+                "Please enter a non-negative integer for the number of samples desired: %d" % n)
+
+        return [self.alias_generation() for i in range(n)]
+
+
+# HELPER FUNCTIONS
+def get_words(file):
+    """ (str) -> list
+    Return a list of words from a given corpus. """
+
+    # Ensure the file is not empty
+    if os.stat(file).st_size == 0:
+        raise IOError(
+            "Please provide a file containing a corpus (not an empty file).")
+
+    # Ensure the file is text based (not binary). This is based on the implementation
+    #  of the Linux file command
+    textchars = bytearray([7, 8, 9, 10, 12, 13, 27]) + \
+        bytearray(range(0x20, 0x100))
+    with open(file, "rb") as bin_file:
+        if bool(bin_file.read(2048).translate(None, textchars)):
+            raise IOError("Please provide a file containing text-based data.")
+
+    with open(file, "r") as corpus:
+        words = corpus.read().split()
+    return words
+
+
+def sample2dist(sample):
+    """ (list) -> dict (i.e {outcome:proportion})
+    Construct a distribution based on an observed sample (e.g. rolls of a bias die) """
+    increment = Decimal(1)/len(sample)
+
+    dist = {}
+    get = dist.get
+    for o in sample:  # o for outcome
+        dist[o] = get(o, 0) + increment
+    return dist
+
+
+# COMMAND LINE HANDLING
+def check_required_arguments(opts, parser):
+    missing_options = []
+    for option in parser.option_list:
+        if re.match(r'^\[REQUIRED\]', option.help) and eval('opts.' + option.dest) is None:
+            missing_options.extend(option._long_opts)
+    if len(missing_options) > 0:
+        parser.error('Missing REQUIRED parameters: ' + str(missing_options))
+        parser.print_help()
+        sys.exit(1)
+
+
+def handle_options():
+    parser = OptionParser()
+    parser.add_option("-p", "--path", dest="path",
+                      help="[REQUIRED] Path to corpus.", metavar="FILE")
+    parser.add_option("-n", "--num", dest="n", type=int,
+                      help="[REQUIRED] Non-negative integer specifying how many samples are desired.", metavar="INT")
+
+    (options, args) = parser.parse_args()
+    check_required_arguments(options, parser)
+    return options
+
+
+def main():
+    # Handle command line arguments
+    options = handle_options()
+
+    try:
+        # Construct distribution
+        words = get_words(options.path)
+        word_dist = sample2dist(words)
+        VA_words = VoseAlias(word_dist)
+
+        # Sample n words
+        print("\nGenerating %d random samples:\n" % options.n)
+        sample = VA_words.sample_n(options.n)
+        for s in sample:
+            print(s)
+    except Exception as e:
+        sys.exit("\nError: %s" % e)
+
+
+if __name__ == "__main__":
+    main()
