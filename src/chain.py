db55b3798b91c040be429414af84cbe33690fc02
imthaghost <imthaghost@protonmail.com>
2019-12-09 01:38:38 -0800

    [update] n-th order markov chain

---
+++ src/chain.py
@@ -0,0 +1,202 @@
+"""chain is small Markov chain generator"""
+
+# Built-in Python Modules
+import random
+import re
+
+# External Python Modules
+from quote import quote
+import pandas as pd
+
+
+class MarkovChain:
+    """Markov chain generator
+    Attributes:
+    - state (str): The current state in the chain
+    - encoder (ListEncoder): The object that manages and transforms the sequence
+    - matrix (list): The transition matrix that drives the chain
+    Methods:
+    - next: Generate the next state
+    Examples:
+    ```
+    sequence = [1, 1, 2, 3, 2, 1, 2, 1, 3, 1]
+    chain = MarkovChain(sequence)
+    chain.next(1)
+    # 2
+    next(chain)
+    # 1
+    chain.next(3, n=5)
+    # [2, 1, 3, 2, 1]
+    ```
+    """
+
+    def __init__(self, sequence):
+        """Params:
+        - sequence (list): Sequence to convert into a Markov chain
+        """
+        self.encoder = ListEncoder()
+        encoded_sequence = self.encoder.fit_transform(sequence)
+        self.matrix = self._sequence_to_matrix(encoded_sequence)
+        self.state = None
+
+    def __repr__(self):
+        return "MarkovChain()"
+
+    def __iter__(self):
+        return self
+
+    def __dir__(self):
+        return ["__next__", "state", "encoder", "matrix", "next"]
+
+    def __next__(self):
+        self.state = self.next(self.state)
+        return self.state
+
+    def show_chain(self):
+        df = pd.DataFrame(
+            self.matrix,
+            index=self.encoder.index_,
+            columns=self.encoder.index_
+        )
+        print(df)
+
+    def next(self, state=None, n=1):
+        """Generate the next state
+        Params:
+        - state (str, optional): The starting/seed state
+        - n (int): Number of states to generate
+        Examples:
+        ```
+        sequence = [1, 1, 2, 3, 2, 1, 2, 1, 3, 1]
+        chain = MarkovChain(sequence)
+        chain.next(1)
+        # 2
+        chain.next(3, n=5)
+        # [2, 1, 3, 2, 1]
+        ```
+        """
+        if state:
+            self.state = state
+        else:
+            possible = list(self.encoder.index_.keys())
+            self.state = random.choice(possible)
+        if n == 1:
+            self.state = self._generate_next(self.state)
+            return self.state
+        else:
+            states = []
+            for _ in range(n):
+                self.state = self._generate_next(self.state)
+                states.append(self.state)
+            return states
+
+    def _generate_next(self, current_state):
+        encoded_state = self.encoder.transform(current_state)
+        probs = self.matrix[encoded_state]
+        next_state = random.choices(range(len(probs)), weights=probs)[0]
+        next_state = self.encoder.inverse_transform(next_state)
+        return next_state
+
+    @staticmethod
+    def _sequence_to_matrix(sequence):
+        unique = len(set(sequence))
+        matrix = [[0] * unique for _ in range(unique)]
+        for (x, y) in zip(sequence, sequence[1:]):
+            matrix[x][y] += 1
+        for row in matrix:
+            rsum = sum(row)
+            if rsum > 0:
+                row[:] = [i / rsum for i in row]
+        return matrix
+
+
+class ListEncoder:
+    """List to Integer Transformer
+    Methods:
+    - fit: Create/learn the integer index lookup
+    - transform: Transform the list according to the index
+    - fit_transform: fit and transform
+    - inverse_transform: Reverse the index lookup
+    Attributes:
+    - index_: Integer lookup learned by fit
+    Example:
+    ```
+    encoder = ListEncoder()
+    sequence = ['rock', 'paper', 'scissors', 'scissors', 'paper']
+    encoder.fit_transform(sequence)
+    # [1, 2, 0, 0, 2]
+    encoder.inverse_transform(1)
+    # 'rock'
+    ```
+    """
+
+    def __repr__(self):
+        return "ListEncoder()"
+
+    def __dir__(self):
+        return ["fit", "transform", "fit_transform", "index_"]
+
+    def fit(self, y):
+        """Fit the Transformer
+        Params:
+        - y (list): Collection to index
+        """
+        self.index_ = {v: k for k, v in enumerate(set(y))}
+        self.decoder = {v: k for k, v in self.index_.items()}
+        return self
+
+    def transform(self, y):
+        """Transform according to the learned index
+        Params:
+        - y (str|list): Item or collection to transform
+        """
+        if not isinstance(y, list):
+            return self.index_.get(y)
+        return [self.index_.get(yi) for yi in y]
+
+    def fit_transform(self, y):
+        """Fit and transform
+        Params:
+        - y (list): Collection to fit and transform
+        """
+        self.fit(y)
+        return self.transform(y)
+
+    def inverse_transform(self, y):
+        """Reverse the transform
+        Params:
+        - y (str|list): Item or collection to reverse lookup
+        """
+        if not isinstance(y, list):
+            return self.decoder.get(y)
+        return [self.decoder.get(yi) for yi in y]
+
+
+if __name__ == "__main__":
+    quotes = quote('shakespeare', 3)
+    print(quotes[0])
+
+    # {'author': 'William Shakespeare',
+    #  'book': 'As You Like It',
+    #  'quote': 'The fool doth think he is wise, but the wise man knows himself to be a fool.'}
+
+    text = '\n'.join([q['quote'] for q in quotes])
+    text = text.lower()
+
+    tokens = re.findall(r"[\w']+|[.,!?;]", text)
+    tokens[:5]
+
+    # ['the', 'fool', 'doth', 'think', 'he']
+
+    chain = MarkovChain(tokens)
+
+    def generate_sentences(chain, n=2, length=(10, 20)):
+        for _ in range(n):
+            l = random.randint(length[0], length[1])
+            nonsense = ' '.join(chain.next(n=l))
+            print(nonsense)
+
+    generate_sentences(chain)
+
+    # and unless by some are fascinated by the hour upon the wind faithful
+    # those that hath had a very much as flaws go
--- src/markovchain.py
+++ src/markovchain.py
@@ -1,55 +1,103 @@
-import numpy as np
+
+""" Markov Chain generator
+
+In mathematical terms, a Markov Chain is a sequence of values 
+where the next value depends only on the current value (and not past values). 
+It's basically a really simple state machine, where 
+given the present state, the future state is conditionally independent of the past.
+Markov chains have many real-world applications. For example, Google's Page Rank 
+algorithm is essentially a Markov chain over a graph of the web.
+One of the simplest and most well known applications of Markov Chains 
+is generating "realistic" looking texts based on some set of input texts.
+In the case of text, a Markov Chain could be used to answer the question, 
+"Given the present word (or set of words), which words might possibly follow?". 
+You could also use Markov Chains to answer the question, 
+"Given the present word, how likely is it that this word I've chosen would be the next?".
+"""
+
+# Built-in Python Modules
+import re
+import random
+from collections import defaultdict, deque
+# Local Python Modules
 
 
-class MarkovChain(dict):
-    def __init__(self, transition_prob):
+class MarkovChain:
+
+    def __init__(self, num_key_words=2):
         """
-        Initialize the MarkovChain instance.
-
-        Parameters
-        ----------
-        transition_prob: dict
-            A dict object representing the transition 
-            probabilities in Markov Chain. 
-            Should be of the form: 
-                {'state1': {'state1': 0.1, 'state2': 0.4}, 
-                 'state2': {...}}
+        :param: num_key_words 
+            :type: int - default 2
+            :description: is the number of words that compose a key (suggested: 2 or 3)
         """
-        super().__init__()
-        self.transition_prob = transition_prob
-        self.states = list(transition_prob.keys())
+        self.num_key_words = num_key_words
+        self.lookup_dict = defaultdict(list)
+        self._punctuation_regex = re.compile('[/,.!;\?\:\-\[\]\(\)\"\\\n]+')
+        self._seeded = False
+        self.__seed_me()
 
-    def next_state(self, current_state):
-        """
-        Returns the state of the random variable at the next time 
-        instance.
+    def __seed_me(self, rand_seed=None):
+        if self._seeded is not True:
+            try:
+                if rand_seed is not None:
+                    random.seed(rand_seed)
+                else:
+                    random.seed()
+                self._seeded = True
+            except NotImplementedError:
+                self._seeded = False
 
-        Parameters
-        ----------
-        current_state: str
-            The current state of the system.
-        """
-        return np.random.choice(
-            self.states,
-            p=[self.transition_prob[current_state][next_state]
-               for next_state in self.states]
-        )
+    """
+  " Build Markov Chain from data source.
+  " Use add_file() or add_string() to add the appropriate format source
+  """
 
-    def generate_states(self, current_state, no=10):
-        """
-        Generates the next states of the system.
+    def add_file(self, file_path):
+        content = ''
+        with open(file_path, 'r') as fh:
+            self.__add_source_data(fh.read())
 
-        Parameters
-        ----------
-        current_state: str
-            The state of the current random variable.
+    def add_string(self, str):
+        self.__add_source_data(str)
 
-        no: int
-            The number of future states to generate.
-        """
-        future_states = []
-        for i in range(no):
-            next_state = self.next_state(current_state)
-            future_states.append(next_state)
-            current_state = next_state
-        return future_states
+    def __add_source_data(self, str):
+        clean_str = self._punctuation_regex.sub(' ', str).lower()
+        tuples = self.__generate_tuple_keys(clean_str.split())
+        for t in tuples:
+            self.lookup_dict[t[0]].append(t[1])
+
+    def __generate_tuple_keys(self, data):
+        if len(data) < self.num_key_words:
+            return
+
+        for i in range(len(data) - self.num_key_words):
+            yield [tuple(data[i:i+self.num_key_words]), data[i+self.num_key_words]]
+
+    def generate_text(self, max_length=50):
+        """Generates text based on the data the Markov Chain contains"""
+        # max_length is the maximum number of words to generate
+        context = deque()
+        output = []
+        if len(self.lookup_dict) > 0:
+            self.__seed_me(rand_seed=len(self.lookup_dict))
+
+            idx = random.randint(0, len(self.lookup_dict)-1)
+            chain_head = list(self.lookup_dict.keys())[idx]
+            context.extend(chain_head)
+
+            while len(output) < (max_length - self.num_key_words):
+                next_choices = self.lookup_dict[tuple(context)]
+                if len(next_choices) > 0:
+                    next_word = random.choice(next_choices)
+                    context.append(next_word)
+                    output.append(context.popleft())
+                else:
+                    break
+            output.extend(list(context))
+        return output
+
+
+if __name__ == "__main__":
+    mc = MarkovChain()
+    mc.add_file('/Users/ghost/Projects/tweetGen/corpus/thus.txt')
+    print(mc.generate_text())
